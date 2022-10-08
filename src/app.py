44b1fc7d5c3f7a138813bdbcd7bd34b4105f0f6a
christian0412-perez <193256@ids.puchiapas.edu.mx>
2022-10-08 13:26:23 -0500

    changed to cloud

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 from bson import ObjectId, json_util
 app = Flask(__name__)
 
-app.config['MONGO_URI']='mongodb://localhost/flaskdb'
+app.config['MONGO_URI']='mongodb+srv://christian:vaca234s@cluster0.0zvvssd.mongodb.net/?retryWrites=true&w=majority'
 mongo=PyMongo(app)
 
 @app.route('/tasks',methods=['POST'])
